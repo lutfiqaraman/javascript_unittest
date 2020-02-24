a072c25dcbcf9c844c10e22568398455bf9b72cb
lutfiqaraman <lutfiqaraman@gmail.com>
2020-02-24 21:32:55 +0200

    convert from C to F

---
+++ temperature.js
@@ -0,0 +1,8 @@
+exports.fahrenheitToCelsius = (temp) => {
+    return (temp - 32)/1.8;
+};
+
+exports.celsiusToFahrenheit = (temp) => {
+    return (temp * 1.8) + 32;
+};
+
--- unit_test/paidamount.test.js
+++ unit_test/paidamount.test.js
@@ -1,4 +1,5 @@
 const amount = require("../amount");
+const temperature = require("../temperature");
 
 test("should calculate the total with tip", () => {
     const total = amount.calculateTip(100, 0.1);
@@ -8,4 +9,14 @@ test("should calculate the total with tip", () => {
 test("should calculate with default tip", () => {
     const total = amount.calculateTip(100);
     expect(total).toBe(125);
+});
+
+test("Should convert 32F to 0C", () => {
+    const convertedTemperature = temperature.fahrenheitToCelsius(32);
+    expect(convertedTemperature).toBe(0);
+});
+
+test("Should convert 0C to 32F", () => {
+    const convertedTemperature = temperature.celsiusToFahrenheit(0);
+    expect(convertedTemperature).toBe(32);
 });
